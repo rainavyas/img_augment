72e775c4e9ea543b2888cf43bba23d157ef6a04e
Vyas Raina <sayvaniar@gmail.com>
2022-10-10 23:26:34 +0100

    ivaxi and i peer programmed some data things

--- src/data/data_selector.py
+++ src/data/data_selector.py
@@ -3,14 +3,15 @@ import numpy as np
 import torchvision as tv
 import torchvision.transforms as transforms
 from torch.utils.data import TensorDataset, ConcatDataset
+from torch.utils.data import random_split
 
 def data_sel(args, train=True):
     if train ==True: return train_selector(args)
     else: return test_selector(args)
 
 
-def train_selector(args):
-    return tv.datasets.CIFAR10(root=args.data_dir_path, train=True, transform=transforms.Compose([
+def train_selector(args, val=0.2):
+    ds = tv.datasets.CIFAR10(root=args.data_dir_path, train=True, transform=transforms.Compose([
                     tv.transforms.RandomCrop(size=32, padding=4),
                     tv.transforms.RandomHorizontalFlip(),
                     tv.transforms.ToTensor(),
@@ -19,6 +20,11 @@ def train_selector(args):
                         std=[0.2023, 0.1994, 0.2010], 
                         ),
                     ]), download=True)
+
+    num_val = int(val*len(ds))
+    num_train = len(ds) - num_val
+    train_ds, val_ds = random_split(ds, [num_train, num_val], generator=torch.Generator().manual_seed(42))
+    return train_ds, val_ds
 
 def test_selector(args):
     corr_path = os.path.join(args.data_dir_path, 'CIFAR-10-C')
@@ -56,7 +62,7 @@ def test_selector(args):
 
         corr = args.domain
         for i in range(19):
-            curr_corr_dset = ensorDataset(all_corr[i][ (corr-1)*10000 : corr*10000], labels[(corr-1)*10000 : corr*10000] )
+            curr_corr_dset = TensorDataset(all_corr[i][ (corr-1)*10000 : corr*10000], labels[(corr-1)*10000 : corr*10000] )
             if i == 0:
                 all_corr_dset = curr_corr_dset
             else: 

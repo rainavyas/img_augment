0127b057a8d4f9af9ef3b92e4a9fa0c5eb448635
Vyas Raina <sayvaniar@gmail.com>
2022-10-10 22:49:07 +0100

    pass args

--- eval.py
+++ eval.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
         device = get_default_device()
 
     # Load the test data
-    ds = data_sel(args.data_name, args.data_dir_path, train=False)
+    ds = data_sel(args, train=False)
     dl = torch.utils.data.DataLoader(ds, batch_size=args.bs, shuffle=False)
 
     # Load model

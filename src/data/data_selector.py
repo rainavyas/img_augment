44c1e1e56facdc92c974e3835d62bfc749d39b01
ivaxi0s <ivaxisheth@gmail.com>
2022-10-11 18:44:37 -0400

    broken digits dataloader

--- src/data/data_selector.py
+++ src/data/data_selector.py
@@ -9,22 +9,7 @@ def data_sel(args, train=True):
     if train ==True: return train_selector(args)
     else: return test_selector(args)
 
-
-def train_selector(args, val=0.2):
-    ds = tv.datasets.CIFAR10(root=args.data_dir_path, train=True, transform=transforms.Compose([
-                    transforms.RandomCrop(size=32, padding=4),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    transforms.Normalize(
-                        mean=[0.4914, 0.4822, 0.4465],
-                        std=[0.2023, 0.1994, 0.2010], 
-                        ),
-                    ]), download=True)
-    
-    aug=args.aug
-    if aug == True:
-        for i in range(3):
-            aug_ds = tv.datasets.CIFAR10(root=args.data_dir_path, train=True, transform=transforms.Compose([
+aug_transform = transforms.Compose([
                         transforms.AutoAugment(),
                         transforms.RandomCrop(size=32, padding=4),
                         transforms.RandomHorizontalFlip(),
@@ -33,83 +18,150 @@ def train_selector(args, val=0.2):
                             mean=[0.4914, 0.4822, 0.4465],
                             std=[0.2023, 0.1994, 0.2010], 
                             ),
-                        ]), download=True)
-            if i == 0:
-                full_ds = aug_ds
-            else: 
-                full_ds = ConcatDataset((full_ds, aug_ds))
-        ds = ConcatDataset((full_ds, ds))
+                        ])
+train_transform = transforms.Compose([
+                    transforms.RandomCrop(size=32, padding=4),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=[0.4914, 0.4822, 0.4465],
+                        std=[0.2023, 0.1994, 0.2010], 
+                        ),
+                    ])
+test_transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=[0.4914, 0.4822, 0.4465],
+                        std=[0.2023, 0.1994, 0.2010], 
+                        ),
+                    ])
+
+grayscale_train_transform = transforms.Compose([
+                        transforms.AutoAugment(),
+                        transforms.RandomCrop(size=32, padding=4),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.Grayscale(3),
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            mean=[0.4914, 0.4822, 0.4465],
+                            std=[0.2023, 0.1994, 0.2010], 
+                            ),
+                        ])
+grayscale_test_transform = transforms.Compose([
+                        transforms.Grayscale(3),
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            mean=[0.4914, 0.4822, 0.4465],
+                            std=[0.2023, 0.1994, 0.2010], 
+                            ),
+                        ])
+grayscale_aug_transform = transforms.Compose([
+                        transforms.RandomCrop(size=32, padding=4),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.Grayscale(3),
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            mean=[0.4914, 0.4822, 0.4465],
+                            std=[0.2023, 0.1994, 0.2010], 
+                            ),
+                        ])
+
+def train_selector(args, val=0.2):
+    if args.data_name =='cifar10':
+        ds = tv.datasets.CIFAR10(root=args.data_dir_path, train=True, transform= train_transform, download = True)
+        
+        if args.aug == True:
+            for i in range(3):
+                aug_ds = tv.datasets.CIFAR10(root=args.data_dir_path, train=True, transform = aug_transform, download=True)
+                if i == 0:
+                    full_ds = aug_ds
+                else: 
+                    full_ds = ConcatDataset((full_ds, aug_ds))
+            ds = ConcatDataset((full_ds, ds))
+    if args.data_name =='digits':
+        mnist_path = os.path.join(args.data_path_root, 'MNIST')
+        usps_path = os.path.join(args.data_path_root, 'usps.bz2')
+        svhn_path = os.path.join(args.data_path_root, 'train_32x32.mat')
+        if args.domain == 'mnist': return datasets.mnist.MNIST(args.data_path_root, train=True, download=True, transform = grayscale_train_transform)
+        elif args.domain == 'svhn': return datasets.svhn.SVHN(args.data_path_root, split='train', download=True, transform = train_transform)
+        elif args.domain == 'usps': return datasets.usps.USPS(args.data_path_root, train=True, download=True, transform = grayscale_train_transform)
+        if args.aug == True:
+            for i in range(3):
+                aug_ds = tv.datasets.CIFAR10(root=args.data_dir_path, train=True, transform = aug_transform, download=True)
+                if i == 0:
+                    full_ds = aug_ds
+                else: 
+                    full_ds = ConcatDataset((full_ds, aug_ds))
+            ds = ConcatDataset((full_ds, ds))
+
+
+
     num_val = int(val*len(ds))
     num_train = len(ds) - num_val
     train_ds, val_ds = random_split(ds, [num_train, num_val], generator=torch.Generator().manual_seed(42))
     return train_ds, val_ds
 
 def test_selector(args):
-    corr_path = os.path.join(args.data_dir_path, 'CIFAR-10-C')
-    if args.domain == 0:
-        test_ds = tv.datasets.CIFAR10(root=args.data_dir_path, train=False, transform=transforms.Compose([
-                    transforms.ToTensor(),
-                    transforms.Normalize(
-                        mean=[0.4914, 0.4822, 0.4465],
-                        std=[0.2023, 0.1994, 0.2010], 
-                        ),
-                    ]), download=True)
-        return test_ds
-    else:
-        all_corr = []
-        all_corr.append(np.load(os.path.join(corr_path, 'brightness.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'contrast.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'defocus_blur.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'elastic_transform.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'fog.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'frost.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'gaussian_blur.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'gaussian_noise.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'glass_blur.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'impulse_noise.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'jpeg_compression.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'motion_blur.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'pixelate.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'saturate.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'shot_noise.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'snow.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'spatter.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'speckle_noise.npy')))
-        all_corr.append(np.load(os.path.join(corr_path, 'zoom_blur.npy')))
-        labels = np.load(os.path.join(corr_path, 'labels.npy'))
+    if args.data_name =='cifar10':
+        corr_path = os.path.join(args.data_dir_path, 'CIFAR-10-C')
+        if args.domain == 0:
+            test_ds = tv.datasets.CIFAR10(root=args.data_dir_path, train=False, transform=test_transform, download=True)
+            return test_ds
+        else:
+            all_corr = []
+            all_corr.append(np.load(os.path.join(corr_path, 'brightness.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'contrast.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'defocus_blur.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'elastic_transform.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'fog.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'frost.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'gaussian_blur.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'gaussian_noise.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'glass_blur.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'impulse_noise.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'jpeg_compression.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'motion_blur.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'pixelate.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'saturate.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'shot_noise.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'snow.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'spatter.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'speckle_noise.npy')))
+            all_corr.append(np.load(os.path.join(corr_path, 'zoom_blur.npy')))
+            labels = np.load(os.path.join(corr_path, 'labels.npy'))
 
-        all_corr = torch.from_numpy(np.array(all_corr)).type(torch.FloatTensor)
-        labels = torch.LongTensor(labels)
+            all_corr = torch.from_numpy(np.array(all_corr)).type(torch.FloatTensor)
+            labels = torch.LongTensor(labels)
 
-        corr = args.domain
-        for i in range(19):
-            curr_corr_dset = TensorDataset(all_corr[i][ (corr-1)*10000 : corr*10000], labels[(corr-1)*10000 : corr*10000] )
-            if i == 0:
-                all_corr_dset = curr_corr_dset
-            else: 
-                all_corr_dset = ConcatDataset((all_corr_dset, curr_corr_dset))
-            
-        # Apply Transforms to the datasets
-        xs = []
-        ys = []
-        for i in range(len(all_corr_dset)):
-            x, y = all_corr_dset[i]
-            x = torch.permute(x, (2,0,1))
-            x = x / 255
-            T = transforms.Normalize(
-                        mean=[0.4914, 0.4822, 0.4465],
-                        std=[0.2023, 0.1994, 0.2010])
-            x = T(x)
-            xs.append(x)
-            ys.append(y)
-            
+            corr = args.domain
+            for i in range(19):
+                curr_corr_dset = TensorDataset(all_corr[i][ (corr-1)*10000 : corr*10000], labels[(corr-1)*10000 : corr*10000] )
+                if i == 0:
+                    all_corr_dset = curr_corr_dset
+                else: 
+                    all_corr_dset = ConcatDataset((all_corr_dset, curr_corr_dset))
+                
+            # Apply Transforms to the datasets
+            xs = []
+            ys = []
+            for i in range(len(all_corr_dset)):
+                x, y = all_corr_dset[i]
+                x = torch.permute(x, (2,0,1))
+                x = x / 255
+                T = transforms.Normalize(
+                            mean=[0.4914, 0.4822, 0.4465],
+                            std=[0.2023, 0.1994, 0.2010])
+                x = T(x)
+                xs.append(x)
+                ys.append(y)
+                
 
-        xs = torch.stack(xs, dim=0)
-        ys = torch.stack(ys, dim=0)
-        all_corr_dset = TensorDataset(xs, ys)
+            xs = torch.stack(xs, dim=0)
+            ys = torch.stack(ys, dim=0)
+            all_corr_dset = TensorDataset(xs, ys)
 
 
-        return all_corr_dset
+            return all_corr_dset
 
 
 

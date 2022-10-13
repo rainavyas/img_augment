20aa2996ee58d7fdba2c4ba4ff48ca434488cc9b
Vyas Raina <sayvaniar@gmail.com>
2022-10-13 01:03:52 +0100

    class for training with density sampling

---
+++ src/training/density_sampling.py
@@ -0,0 +1,6 @@
+class DensitySampleTrainer():
+    '''
+    Use a training set (e.g augmented training points) to learn a density distribution
+    For a desired training set (e.g. without augmentation) calculate likelihood
+    
+    '''

06f5f78e282ab4729845e29f0a282726f94e6f34
ivaxi0s <ivaxisheth@gmail.com>
2022-11-24 16:00:53 -0500

    modified analyze with s and p and both viz

--- analyze.py
+++ analyze.py
@@ -12,11 +12,20 @@ import matplotlib.pyplot as plt
 from src.training.density_estimator import Estimator
 from src.data.data_selector import data_sel
 
+def get_weights(kde_dist, ds):
+    #  return normalised likelihood for ds samples as per kde_dist
+    log_weights = Estimator.test_kde(ds, kde_dist)
+    scaled_log_weights = log_weights - np.max(log_weights)
+    weights = np.exp(scaled_log_weights)
+    weights = weights/np.sum(weights)
+    return weights
+
 if __name__ == "__main__":
 
     # Get command line arguments
     commandLineParser = argparse.ArgumentParser()
     commandLineParser.add_argument('--weights_dist_learn', action='store_true', help='save weights distribution')
+    commandLineParser.add_argument('--weights_dist_type', type=str, choices=['p', 's', 'both'], default='both', help='which weight distribution to save')
     commandLineParser.add_argument('--weights_dist_visualize', action='store_true', help='visualize saved weights distribution')
     commandLineParser.add_argument('--data_name', type=str, default='cifar10', help='e.g. cifar10')
     commandLineParser.add_argument('--data_dir_path', type=str, default='data', help='path to data directory, e.g. data')
@@ -28,7 +37,11 @@ if __name__ == "__main__":
     commandLineParser.add_argument('--load_files', type=str, default=[], nargs='+', help="paths of files to load")
     commandLineParser.add_argument('--names', type=str, default=[], nargs='+', help="name for each file in legend")
     commandLineParser.add_argument('--plot_file', type=str, default='.', help="path to save plot")
-    commandLineParser.add_argument('--xclip', type=float, default=0.0, help="if you want to clip the plot for visualizaion")
+    commandLineParser.add_argument('--xhigh', type=float, default=0.0, help="if you want to clip the plot for visualizaion")
+    commandLineParser.add_argument('--xlow', type=float, default=0.0, help="if you want to clip the plot for visualizaion")
+    commandLineParser.add_argument('--aug_num', type=int, default=3, help="Number of times to augment")
+    commandLineParser.add_argument('--gamma', type=float, default=1.0, help="Importance weighting power")
+    
     args = commandLineParser.parse_args()
 
     # Save the command run
@@ -49,14 +62,27 @@ if __name__ == "__main__":
         train_ds, _ = data_sel(args, train=True)
 
         # get weights
-        dist_model = Estimator.train_kde(ds_for_dist, kernel='gaussian', bandwidth=args.B)
-        log_weights = Estimator.test_kde(train_ds, dist_model)
-        scaled_log_weights = log_weights - np.max(log_weights)
-        weights = np.exp(scaled_log_weights)
+        if args.weights_dist_type == 'both' or args.weights_dist_type == 'p':
+            dist_model = Estimator.train_kde(ds_for_dist, kernel='gaussian', bandwidth=args.B)
+            dist_weights = get_weights(dist_model, train_ds) # p(x)
+        
+        if args.weights_dist_type == 'both' or args.weights_dist_type == 's':
+            train_dist_model = Estimator.train_kde(train_ds, kernel='gaussian', bandwidth=args.B)
+            train_weights = get_weights(train_dist_model, train_ds) # s(x)
+        
+        if args.weights_dist_type == 'both':
+            corrected_weights = dist_weights / train_weights # p(x)/s(x) = w
+            corrected_weights = corrected_weights**args.gamma # p(x)/s(x)**gamma
 
         # save weights
-        out_file = f'{args.save_dir}/weights_dist_only_aug{args.only_aug}_domain{args.domain}_{args.data_name}_B{args.B}'
-        np.save(out_file, weights)
+        out_file = f'{args.save_dir}/weights_dist_{args.weights_dist_type}_domain{args.domain}_{args.data_name}_B{args.B}_gamma{args.gamma}'
+        if args.weights_dist_type=='both':
+            np.save(out_file, corrected_weights)
+        elif args.weights_dist_type=='p':
+            np.save(out_file, dist_weights)
+        elif args.weights_dist_type=='s':
+            np.save(out_file, train_weights)
+        
     
     if args.weights_dist_visualize:
         # cumulative histogram plot of weights
@@ -65,18 +91,14 @@ if __name__ == "__main__":
         for fpath, name in zip(args.load_files, args.names):
             weights = np.load(fpath)
             weights = np.sort(weights)
-            try:
-                weights = weights/np.sum(weights)
-            except:
-                pass
-
+        
             plt.plot(weights, np.arange(weights.size)/weights.size, label=name)
         
-        plt.legend(title='Bandwidth')
+        plt.legend(title='Gamma') # earlier Bandwidth
         plt.xlabel('Normalized Weight')
         plt.ylabel('Cumulative Density')
-        if args.xclip > 0.0:
-            plt.xlim([-0.00001, args.xclip])
+        if args.xhigh > 0.0:
+            plt.xlim([args.xlow, args.xhigh])
         plt.savefig(args.plot_file, bbox_inches='tight')
 
 

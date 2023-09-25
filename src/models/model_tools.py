49e3b50a2fa74568bfa417379eee815455cf2ed4
ivaxi0s <32988966+ivaxi0s@users.noreply.github.com>
2023-09-25 20:00:23 +0200

    replaced batched with xs

--- src/models/model_tools.py
+++ src/models/model_tools.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn.functional as F
 
-def get_cam_pred(model, batch):
-    features = model._features(batch)
+def get_cam_pred(model, xs):
+    features = model._features(xs)
     model_fc = model._classifier
     cam = F.conv2d(features, model_fc.weight.view(model_fc.out_features, features.size(1), 1, 1)) + model_fc.bias.unsqueeze(0).unsqueeze(2).unsqueeze(3)
     return cam
